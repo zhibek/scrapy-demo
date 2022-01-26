347f99ac3dcd3074be30a326e4e226c715adfe33
John Levermore <john.levermore@zhibek.com>
2022-01-26 22:39:06 +0600

    Experiment with scrapy_proxy_pool

--- project/project/settings.py
+++ project/project/settings.py
@@ -86,3 +86,10 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+PROXY_POOL_ENABLED = True
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+    'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+}
